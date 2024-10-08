08e36fd6167f96a14e2c08433a9c3d69ee0dcd6e
JanakaAlwis <amk1004093@student.hamk.fi>
2024-10-08 11:21:17 +0300

    Define age groups 15 to 17 and 18
    Check whether which vehicle they can drive.

--- src/App.java
+++ src/App.java
@@ -1,15 +1,23 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        int age = 10; 
+        int age = 18; 
         if (age > 0 && age < 18) 
         { 
+            if (age >= 15) 
+            {
+                System.out.println("You can drive a moped."); 
+            }
             System.out.println("You are underage."); 
         } 
         else if (age >= 65) 
         { 
             System.out.println("You are retired."); 
         }
-        else 
+        else if (age == 18)
+        {
+            System.out.println("You can drive a car."); 
+        }
+        else
         { 
             System.out.println("You are an adult."); 
         } 
